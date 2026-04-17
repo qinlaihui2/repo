0d1ff1314b16e30410082cebd7d2facaa35f201b
qinlaihui2 <2188926911@qq.com>
2026-04-17 22:55:57 +0800

    Add files via upload
    前端页面

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter, Playfair_Display, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const playfair = Playfair_Display({
+  variable: "--font-playfair",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MyBlog | 清新杂志风博客",
+  description: "清新杂志风全栈个人博客",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="zh-CN"
+      className={cn("h-full", "antialiased", inter.variable, playfair.variable, "font-sans", geist.variable)}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
