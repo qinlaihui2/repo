f7c9a60d70d484b0b6e71e7e3e07138410476570
qinlaihui2 <2188926911@qq.com>
2026-04-18 14:29:37 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["react-markdown", "remark-gfm"],
+  /* config options here */
 };
 
 export default nextConfig;
